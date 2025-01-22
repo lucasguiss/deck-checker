6dbbf2137e610964526a98c25e74d2ed0b70cc69
Lucas Guiss <lucasguissgusmao@gmail.com>
2025-01-22 20:40:20 -0300

    chore: add github pages workflows

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  basePath: "/dech-checker",
+  reactStrictMode: true,
 };
 
 export default nextConfig;
